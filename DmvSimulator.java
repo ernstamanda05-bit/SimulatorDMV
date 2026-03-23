c77747c5f8d9279cfaa5559f3c8b9e32de3f234e
ernstamanda05-bit <ernstamanda05@gmail.com>
2026-03-23 17:20:09 -0400

    Initial DMV simulator

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+
 public class DmvSimulator {
     public static void main(String[] args) {
-     
+        Random rand = new Random();
+
+        System.out.println("Welcome to the DMV!");
+
+        int userNumber = rand.nextInt(100) + 1;
+        System.out.println("Your number is: " + userNumber);
+        System.out.println("Please wait until your number is called...\n");
+
+        int current = userNumber + 1;
+
+        while (true) {
+            if (current > 100) {
+                current = 1;
+            }
+
+            if (current == userNumber) {
+                System.out.println("Now serving number: " + current);
+                break;
+            }
+
+            System.out.println("Now serving number: " + current);
+            current++;
+        }
+
+        System.out.println("Sorry, you don't have the required paperwork.");
     }
 }
